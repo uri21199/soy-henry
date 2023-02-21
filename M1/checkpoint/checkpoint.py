e4acbf4df554ea240dbd29674a0e79b549d3b1b8
Lautaro Bustos <uri21199@gmail.com>
2023-02-20 22:55:30 -0300

    checkpoints nuevos

--- M1/checkpoint/checkpoint.py
+++ M1/checkpoint/checkpoint.py
@@ -10,94 +10,84 @@ def Ret_Pregunta01():
     '''
     Debes utilizar Pandas para ingestar en un objeto Dataframe el contenido del archivo provisto
     "Fuentes_Consumo_Energia.csv".
-    Esta función debe informar la cantidad de registros cuya entidad sean Colombia o México retornando ese valor en un dato
-    tupla (Colombia, México).
+    Esta funcion debe informar la cantidad de registros cuya entidad sean Colombia o Mexico retornando ese valor en un dato
+    tupla (Colombia, Mexico).
     Pista averiguar la funcion Shape
     '''
-    #Tu código aca:
-
-    df = pd.read_csv("datasets/Fuentes_Consumo_Energia.csv")
-    
-    colombia= df[df['Entity'].isin(['Colombia'])].shape
-    mexico= df[df['Entity'].isin(['Mexico'])].shape
-
-    return (colombia[0],mexico[0])
+    #Tu codigo aca:
+    df = pd.read_csv("./datasets/Fuentes_Consumo_Energia.csv")
+    colombia = df[df["Entity"] == "Colombia"].shape
+    print(colombia)
+    mexico = df[df["Entity"] == "Mexico"].shape #Traemos (filas, columnas)
+    print(mexico)
+    tupla = (colombia[0], mexico[0])
+    return tupla
 
 def Ret_Pregunta02():
     '''
     Debes utilizar Pandas para ingestar en un objeto Dataframe el contenido del archivo provisto
     "Fuentes_Consumo_Energia.csv".
-    Esta función debe eliminar las columnas 'Code' y 'Entity' y luego informar la cantidad de columnas
+    Esta funci�n debe eliminar las columnas 'Code' y 'Entity' y luego informar la cantidad de columnas
     retornando ese valor en un dato de tipo entero.
     '''
-    #Tu código aca:
-
+    #Tu c�digo aca:
     df = pd.read_csv("./datasets/Fuentes_Consumo_Energia.csv")
-
-    df.drop(["Code"], axis="columns", inplace= True)
-    df.drop(["Entity"], axis="columns", inplace= True)
-    cantidad_columnas = df.shape
-
-    return cantidad_columnas[1] 
+    df.drop(['Code','Entity'], axis= 1, inplace = True)
+    #print(df.head(1))
+    
+    return df.shape[1]
 
 def Ret_Pregunta03():
     '''
     Debes utilizar Pandas para ingestar en un objeto Dataframe el contenido del archivo provisto
     "Fuentes_Consumo_Energia.csv".
-    Esta función debe informar la cantidad de registros de la columna Year sin tener en cuenta aquellos con valores faltantes
+    Esta funcion debe informar la cantidad de registros de la columna Year sin tener en cuenta aquellos con valores faltantes
     retornando ese valor en un dato de tipo entero.
     '''
     df = pd.read_csv("./datasets/Fuentes_Consumo_Energia.csv")
-    
 
-    return df['Year'].dropna().shape[0]
+    df.dropna()
+    df1 = df['Year'].shape[0]
+    return df1
 
 def Ret_Pregunta04():
     '''
     Debes utilizar Pandas para ingestar en un objeto Dataframe el contenido del archivo provisto
     "Fuentes_Consumo_Energia.csv".
     El ExaJulio es una unidad diferentes al TWh, es decir, no tiene sentido sumarlos o
-    buscar proporciones entre ellos, la fórmula de conversión es:
+    buscar proporciones entre ellos, la formula de conversion es:
     277.778 Teravatios/Hora (TWh) = 1 Exajulio
     Los campos terminados en "_EJ" corresponden a mediciones en Exajulios,
     y los terminados en "_TWh" corresponden a Teravatios/Hora.
     La consigna es crear un nuevo campo, que se denomine "Consumo_Total"
     y que guarde la sumatoria de todos los consumos expresados en Teravatios/Hora
-    (convirtiendo a esta medida los que están en Exajulios)
-    Esta función debe informar el consumo total para la entidad 'World' y año '2019',
+    (convirtiendo a esta medida los que estan en Exajulios)
+    Esta funcion debe informar el consumo total para la entidad 'World' y año '2019',
     redondeado a 2 decimales, retornando ese valor en un dato de tipo float.
     '''
-    #Tu código aca:
+    #Tu codigo aca:
     df = pd.read_csv("./datasets/Fuentes_Consumo_Energia.csv")
 
-    df['Consumo_Total'] = (df.Coal_Consumption_EJ*277.778 + 
-                          df.Gas_Consumption_EJ*277.778 +
-                          df.Geo_Biomass_Other_TWh +
-                          df.Hydro_Generation_TWh +
-                          df.Nuclear_Generation_TWh +
-                          df.Solar_Generation_TWh +
-                          df.Wind_Generation_TWh +
-                          df.Oil_Consumption_EJ*277.778)
+    df["Consumo_Total"] = ((df["Coal_Consumption_EJ"] + df["Gas_Consumption_EJ"] + df["Oil_Consumption_EJ"]) * 277.778 + df["Geo_Biomass_Other_TWh"] + df["Wind_Generation_TWh"] + df["Hydro_Generation_TWh"] + df["Solar_Generation_TWh"] + df["Nuclear_Generation_TWh"])
 
-    return round(float(df[(df.Entity == 'World') & (df.Year == 2019)]['Consumo_Total'].values),2)
+    df = df[(df["Entity"] == "World") & (df["Year"] == 2019)]
+
+    return round(float(df['Consumo_Total']), 2)
 
 def Ret_Pregunta05():
     '''
     Debes utilizar Pandas para ingestar en un objeto Dataframe el contenido del archivo provisto
     "Fuentes_Consumo_Energia.csv".
-    Esta función debe informar el año de mayor generación de energía hídrica (Hydro_Generation_TWh)
+    Esta funcion debe informar el año de mayor generacion de energia hidrica (Hydro_Generation_TWh)
     para la entidad 'Europe' retornando ese valor en un dato de tipo entero.
     '''
-    #Tu código aca:
+    #Tu c�digo aca:
     df = pd.read_csv("./datasets/Fuentes_Consumo_Energia.csv")
+    df = df[df["Entity"] == "Europe"]
+    indice = df["Hydro_Generation_TWh"].idxmax()
+    #print(indice)
 
-    mask = df["Entity"] == "Europe"
-
-    df = df[mask]
-
-    indice = df.Hydro_Generation_TWh.idxmax() 
-
-    return df.Year[indice]
+    return df["Year"][indice]
 
 def Ret_Pregunta06(m1, m2, m3):
     '''
